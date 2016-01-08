1630fc87d77509a4705a78a8c13cec2733599b3d
Jerry Zou <jerry.zry@outlook.com>
2016-01-08 13:48:27 +0800

    Playback demo

---
+++ KinectPlayback/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Kinect.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KinectPlayback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaybackClip("D:\\Dev\\Kinect\\20160108_043343_00.xef", 1);
+        }
+
+        public static void PlaybackClip(string filePath, uint loopCount)
+        {
+            using (KStudioClient client = KStudio.CreateClient())
+            {
+                client.ConnectToService();
+
+                using (KStudioPlayback playback = client.CreatePlayback(filePath))
+                {
+                    playback.LoopCount = loopCount;
+                    playback.Start();
+
+                    while (playback.State == KStudioPlaybackState.Playing)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    if (playback.State == KStudioPlaybackState.Error)
+                    {
+                        throw new InvalidOperationException("Error: Playback failed!");
+                    }
+                }
+
+                client.DisconnectFromService();
+            }
+        }
+    }
+}
